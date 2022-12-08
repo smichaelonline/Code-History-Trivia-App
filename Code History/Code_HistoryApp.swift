164f08390cbd194bf808e89e4e7e2144205fa266
smichaelonline <stephaniemichael001@gmail.com>
2022-12-08 12:10:26 -0500

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Stephanie Michael on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
