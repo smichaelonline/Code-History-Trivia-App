39452a556c6b2a85d1e9d4d0f4ba1710b940aae6
smichaelonline <stephaniemichael001@gmail.com>
2022-12-27 13:28:22 -0500

    Add all buttons and test functionality

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -35,6 +35,36 @@ struct ContentView: View {
                             .padding()
                             .border(accentColor, width: 4)
                     })
+                    Button(action: {
+                        print("Tapped on Choice 2")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
                 }
             }
         }
