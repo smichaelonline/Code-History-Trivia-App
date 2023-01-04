ebb1babfadedd6973dbcfa8812ad69c8f12bbcd3
smichaelonline <stephaniemichael001@gmail.com>
2023-01-04 16:49:33 -0500

    Add navigation view to welcome screen

--- Code History/Views/WelcomeView.swift
+++ Code History/Views/WelcomeView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                mainColor.ignoresSafeArea()
+                VStack{
+                    Spacer()
+                    VStack(alignment: .leading, spacing: 0){
+                        Text("Select the correct answers to the following questions.")
+                            .font(.largeTitle)
+                            .bold()
+                            .multilineTextAlignment(.leading)
+                            .padding()
+                    }
+                    }
+                    Spacer()
+                    NavigationLink(
+                        destination: GameView(),
+                        label: {
+                            HStack {
+                                Spacer()
+                                Text("Okay, let's go!")
+                                    .font(.body)
+                                    .bold()
+                                    .padding()
+                                Spacer()
+                            }.background(accentColor)
+                    })
+                .foregroundColor(.white)
+            }
+        }
     }
 }
 
