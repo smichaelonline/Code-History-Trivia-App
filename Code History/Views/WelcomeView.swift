7dcb35a2bfaefddd0a1907273783c01c17da322b
smichaelonline <stephaniemichael001@gmail.com>
2023-01-04 16:37:49 -0500

    Add welcome view for users to see welcome page upon entering game

---
+++ Code History/Views/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  Code History
+//
+//  Created by Stephanie Michael on 1/4/23.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
